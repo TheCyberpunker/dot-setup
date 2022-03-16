db09397e9de3148665f02e574ddac761e772ece9
ufo <ufo@localhost.localdomain>
2022-03-16 15:19:52 +0100

    () thecyberpunker@v0.01

--- install.sh
+++ install.sh
@@ -30,6 +30,7 @@ sudo apt install -y arandr arc-theme feh i3blocks i3status i3 i3-wm lxappearance
 sudo apt install -y libxcb-shape0-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev autoconf meson
 sudo apt install -y libxcb-render-util0-dev libxcb-shape0-dev libxcb-xfixes0-dev 
 sudo apt install -y i3-gaps
+sudo apt install -y jq
 #install utilities
 #peek record screen and make gif
 #flameshot screenshots
