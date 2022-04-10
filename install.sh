200340faa78f7afe4d074bf7923333698cfc1ce8
ufo <ufo@bpshirase.ufo>
2022-04-10 19:05:25 -0400

    fix parrot install

--- install.sh
+++ install.sh
@@ -17,12 +17,15 @@ echo -e "##############################################################"
 ## $(whoami) var user
 
 ## sleep 1 // pause
-read -s -n 1 -p "Update the system. Press ANY key to continue.\n"
+read -s -n 1 -p "Update the system. Press ANY key to continue."
+echo -e ""
 sleep 1
 #update
 sudo apt update && sudo parrot-upgrade -y
-echo -e "System updated \n"
-read -s -n 1 -p "Install dependencies. Press ANY key to continue.\n"
+echo -e "System updated"
+echo -e ""
+read -s -n 1 -p "Install dependencies. Press ANY key to continue."
+echo -e ""
 
 sleep 1
 #install dependencies
@@ -31,24 +34,27 @@ sudo apt install -y libxcb-shape0-dev libxcb-keysyms1-dev libpango1.0-dev libxcb
 sudo apt install -y libxcb-render-util0-dev libxcb-shape0-dev libxcb-xfixes0-dev 
 sudo apt install -y i3-gaps
 sudo apt install -y jq
+sudo apt install -y zsh
 #install utilities
 #peek record screen and make gif
 #flameshot screenshots
 sudo apt install -y peek flameshot
 
-echo -e "Dependencies installed \n"
-read -s -n 1 -p "Install fonts - obsidianMD / Brave-browser (nerdfonts/Iosevka,RobotoMono). Press ANY key to continue \n"
-#install obsidianMD
-wget https://github.com/obsidianmd/obsidian-releases/releases/download/v0.12.15/obsidian_0.12.15_amd64.deb
-sudo dpkg -i obsidian_0.12.15_amd64.deb
-sudo chmod 4755 /opt/Obsidian/chrome-sandbox
+echo -e "Dependencies installed"
+echo -e ""
+read -s -n 1 -p "Install fonts - obsidianMD / Brave-browser (nerdfonts/Iosevka,RobotoMono). Press ANY key to continue"
+echo -e ""
+#install problem with new kernels, better used appimage - obsidianMD << FIX THIS
+#wget https://github.com/obsidianmd/obsidian-releases/releases/download/v0.12.15/obsidian_0.12.15_amd64.deb
+#sudo dpkg -i obsidian_0.12.15_amd64.deb
+#sudo chmod 4755 /opt/Obsidian/chrome-sandbox
 
 #install brave-browser
-sudo apt install apt-transport-https curl
+sudo apt install -y apt-transport-https curl
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-sudo apt update
-sudo apt install brave-browser
+sudo apt update -y
+sudo apt install -y brave-browser
 
 sleep 1
 #install fonts
@@ -58,19 +64,23 @@ wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/RobotoMono
 unzip Iosevka.zip -d ~/.local/share/fonts/
 unzip RobotoMono.zip -d ~/.local/share/fonts/
 fc-cache -fv #update fonts cache
-echo -e "Fonts installed \n"
-read -s -n 1 -p "Install Alacritty Terminal, Press ANY key to continue \n"
+echo -e "Fonts installed"
+echo -e ""
+read -s -n 1 -p "Install Alacritty Terminal, Press ANY key to continue"
+echo -e ""
 
 sleep 1
 #install alacritty terminal or other terminal
 wget https://github.com/barnumbirr/alacritty-debian/releases/download/v0.10.1-1/alacritty_0.10.1-1_amd64_bullseye.deb
 sudo dpkg -i alacritty_0.10.1-1_amd64_bullseye.deb
 sudo apt install -f
-echo -e "Alacritty installed \n"
-read -s -n 1 -p "Configure and copy the dot files, Press ANY key to continue \n"
 
+echo -e "Alacritty installed"
+echo -e ""
 pip3 install pywal
-
+echo -e ""
+read -s -n 1 -p "Configure and copy the dot files, Press ANY key to continue"
+echo -e ""
 sleep 1
 #copy the dot config files
 mkdir -p ~/.config/i3
@@ -97,8 +107,14 @@ rm Iosevka.zip
 rm RobotoMono.zip
 rm obsidian_0.12.15_amd64.deb
 
-echo "Done! Grab some wallpaper and run pywal -i filename to set your color scheme. \n"
-echo "To have the wallpaper set on every boot edit ~.fehbg \n"
-echo "After reboot: Select i3 on login, run lxappearance and select arc-dark \n"
-read -s -n 1 -p "Press ANY key to finish \n"
+echo "Done! Grab some wallpaper and run pywal -i filename to set your color scheme."
+echo -e ""
+echo "To have the wallpaper set on every boot edit ~.fehbg"
+echo -e ""
+echo "After reboot: Select i3 on login, run lxappearance and select arc-dark"
+echo -e ""
+read -s -n 1 -p "install zsh, press ANY key to finish"
+echo -e ""
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo -e ""
+echo -e "Reboot Manually >:)"
